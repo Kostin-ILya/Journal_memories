ac5f2cfba09c18536e3a48005fc1972e886942fa
Ilya <lord-ilja@yandex.ru>
2023-10-08 02:56:27 +0300

    add CreateMemoryBtn

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import CreateMemoryBtn from './components/CreateMemoryBtn/CreateMemoryBtn'
 import Header from './layouts/LeftPanel/Header'
 import LeftPanel from './layouts/LeftPanel/LeftPanel'
 
@@ -7,6 +8,7 @@ function App() {
     <>
       <LeftPanel>
         <Header />
+        <CreateMemoryBtn />
       </LeftPanel>
     </>
   )
---
+++ src/components/CreateMemoryBtn/CreateMemoryBtn.jsx
@@ -0,0 +1,33 @@
+import styles from './CreateMemoryBtn.module.scss'
+
+const CreateMemoryBtn = () => {
+  return (
+    <button className={styles['new-record-btn']}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="20"
+        height="20"
+        viewBox="0 0 20 21"
+        fill="none"
+      >
+        <path
+          d="M10 4.96265V16.6293"
+          stroke="white"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M4.16669 10.796H15.8334"
+          stroke="white"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
+      <span className={styles['text']}>Новое воспоминание</span>
+    </button>
+  )
+}
+
+export default CreateMemoryBtn
